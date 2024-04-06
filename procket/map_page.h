f3a836c9caf28bb4312896a7ad1e30f5e6dc969a
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-04-06 10:51:13 +0300

    Uuuuhhhhhhhhhhhhh

--- procket/map_page.h
+++ procket/map_page.h
@@ -1,16 +1,29 @@
 #pragma once
 
+
+#define ElemCout_Debug
+
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <iostream>
 #include <cmath>
 #include "textRendering.h"
 #include "menu.h"
+
+#define FD_DEBUG
+#include "FD_IO.h"
+
 #include "switch_page.h"
 #define MAX_ELEM 1001
 #include <string>
+
+
+int msX[MAX_ELEM], msY[MAX_ELEM]; //for mouse inputs
+
+
 bool map[720][1280];
-int ponr; //TODO: trebuie sa gasim un alt nume pentru aceasta variabila, ca numele asta este o anagrama pentru alt nume, pe care nu il voi spune aici
+int movPos;
 bool finpon = true;
 //Tipuri:
 //1-punct
@@ -26,24 +39,37 @@ bool finpon = true;
 //9-asteroid static 
 //10 - gaura neagra cu diametru maxim
 
-int ctype = 1;
+//Variabile pt numararea si individualizarea elementelor de acelasi tip
+int cntT1 = 0;
+int cntT2 = 0;
+int cntT3 = 0;
+int cntT4 = 0;
+int cntT5 = 0;
+int cntT6 = 0;
+int cntT7 = 0;
+
+
+
+int ctype[MAX_ELEM] = {1};
 int i;
 struct pofint{
   int x;
   int y;
-};   
+}; 
+
+#include "rmap_utils.h"
 
 pofint rpovif[100]; //pentru racheta;
 int nr_rpovif;
 
 struct aelem {
   int type;
-  int d=0; // pentru cerc
-  int dc = 0; //pentru stele (diametru caldura)
-  int dm = 0; //pentru gauri negre (diametru maxim) si pentru stele, pentru a calcula cat de aproape se poate afla racheta de aceasta stea.
-  int temp = 0; //pentru stele: 0-stea rece; 1-stea calduta; 2-stea calda; 3-stea foarte calda; 4-stea fierbinte.
-  int x; // generic
-  int y; // generic 
+  int d=0;           // pentru cerc
+  int dc = 0;        //pentru stele (diametru caldura)
+  int dm = 0;        //pentru gauri negre (diametru maxim) si pentru stele, pentru a calcula cat de aproape se poate afla racheta de aceasta stea.
+  int temp = 0;      //pentru stele: 0-stea rece; 1-stea calduta; 2-stea calda; 3-stea foarte calda; 4-stea fierbinte.
+  int x;             // generic
+  int y;             // generic 
   short cycle = 0;
   int nrpovif = 0;
   pofint povif[100]; //pentru planeta mergatoare, asteroid si gauri negre
@@ -57,55 +83,62 @@ void RenderMainMenu();
 
 
 void MakeElem(int X, int Y){
-  if(ctype == 1){
+  if(ctype[nr_elem] == 1){
     E[nr_elem].type = 1;
+    cntT1 ++;
 }
-  if(ctype == 2){
+  if(ctype[nr_elem] == 2){
     E[nr_elem].type = 2;
     E[nr_elem].d = 25;
+    cntT2 ++;
 }
-  if(ctype == 3 || ctype == 5){
+  if(ctype[nr_elem] == 3 || ctype[nr_elem] == 5){
     if(finpon){
-   if(ctype == 3){   E[nr_elem].type = 3;}
+   if(ctype[nr_elem] == 3){   E[nr_elem].type = 3;}
       else{ E[nr_elem].type = 5; }
       E[nr_elem].d = 25;
-      E[nr_elem].x = E[nr_elem-ponr].x;
-      E[nr_elem].y = E[nr_elem-ponr].y;
-      for(int i=0;i<ponr;i++){
-        E[nr_elem].povif[i].x = E[nr_elem - ponr + i].x;
-        E[nr_elem].povif[i].y = E[nr_elem - ponr + i].y;
+      E[nr_elem].x = E[nr_elem-movPos].x;
+      E[nr_elem].y = E[nr_elem-movPos].y;
+      for(int i=0;i<movPos;i++){
+        E[nr_elem].povif[i].x = E[nr_elem - movPos + i].x;
+        E[nr_elem].povif[i].y = E[nr_elem - movPos + i].y;
       }
       
-      E[nr_elem].nrpovif = ponr;
-      ponr = 0;
+      E[nr_elem].nrpovif = movPos;
+      movPos = 0;
     }
     else{
       E[nr_elem].type = 8;
-      ponr++;
+      movPos++;
     }
 
+    cntT3 ++;
+
   }
-  if(ctype == 6){
+  if(ctype[nr_elem] == 6){
     E[nr_elem].type = 6;
     E[nr_elem].x = X/2;
     E[nr_elem].y = Y/2;
     rpovif[nr_rpovif].x = X/2;
     rpovif[nr_rpovif].y = Y/2;
     nr_rpovif++;
+    cntT6 ++;
   }
-  if(ctype == 7){
+  if(ctype[nr_elem] == 7){
     E[nr_elem].type = 7;
     E[nr_elem].d = 4;
     E[nr_elem].dm = E[nr_elem].d*16;
     E[nr_elem].cycle = 2;
+    cntT7 ++;
   }
-  if(ctype == 4){
+  if(ctype[nr_elem] == 4){
     E[nr_elem].type = 4;
     E[nr_elem].temp = 2;
     E[nr_elem].d = 15;
     E[nr_elem].dm = E[nr_elem].d * E[nr_elem].temp + 5;
+    cntT4 ++;
   }
-if(ctype == 1 || ctype == 2 || (ctype == 3 && !finpon) || (ctype == 5 && !finpon) || ctype == 7 || ctype == 4){
+if(ctype[nr_elem] == 1 || ctype[nr_elem] == 2 || (ctype[nr_elem] == 3 && !finpon) || (ctype[nr_elem] == 5 && !finpon) || ctype[nr_elem] == 7 || ctype[nr_elem] == 4){
   E[nr_elem].x = X/2;
   E[nr_elem].y = Y/2;
 }
@@ -122,9 +155,9 @@ bool IsPollingEventM() {
         switch (WindowEvent.button.button)
         {
             case SDL_BUTTON_LEFT:
-                SDL_GetMouseState(&msX, &msY);
-                std::cout <<  msX<<" "<<msY<<'\n';
-                MakeElem(msX, msY);
+                SDL_GetMouseState(&msX[nr_elem], &msY[nr_elem]);
+                std::cout << "Mouse pos on click: " <<  msX<<" "<<msY<<'\n';
+                MakeElem(msX[nr_elem], msY[nr_elem]);
                 nr_elem++;
                 break;
             
@@ -135,40 +168,68 @@ bool IsPollingEventM() {
           
             if(WindowEvent.key.keysym.sym == SDLK_1)
               if(finpon)
-              ctype = 1;
+              ctype[nr_elem] = 1;
             if(WindowEvent.key.keysym.sym == SDLK_2)
               if(finpon)
-              ctype = 2;
+              ctype[nr_elem] = 2;
             if(WindowEvent.key.keysym.sym == SDLK_3)
-              if(ponr == 0 && finpon){
-                 ctype = 3;
+              if(movPos == 0 && finpon){
+                 ctype[nr_elem] = 3;
                  finpon = false;
               }
             if(WindowEvent.key.keysym.sym == SDLK_4)
-              ctype = 4;
+              ctype[nr_elem] = 4;
             if(WindowEvent.key.keysym.sym == SDLK_6)
-              ctype = 6;
+              ctype[nr_elem] = 6;
             if(WindowEvent.key.keysym.sym == SDLK_5)
-              if(ponr == 0 && finpon){
-              ctype = 5;
+              if(movPos == 0 && finpon){
+              ctype[nr_elem] = 5;
               finpon = false;
               }
             if(WindowEvent.key.keysym.sym == SDLK_7)
-              ctype = 7;
+              ctype[nr_elem] = 7;
+
+            if(WindowEvent.key.keysym.sym == SDLK_s)
+            {
+              SaveFileDialog();
+              WriteRMAP(rmap_o);
+            }
             if(WindowEvent.key.keysym.sym == SDLK_LEFT)
             {
               SDL_RenderSetScale(Renderer, 1, 1);
                 ClearMainGraphics();
                 RenderMainMenu();
             }
-            std::cout <<'\n'<< "ctype: "<<ctype<<'\n';
-            if(WindowEvent.key.keysym.sym == SDLK_RETURN && !finpon && ponr > 0){
+            std::cout <<'\n'<< "ctype: "<<ctype[nr_elem]<<'\n';
+            #ifdef ElemCout_Debug
+              std::cout << "Element Count: " << nr_elem << "\n";
+              std::cout << "Elem type1: " << cntT1 << "\n";
+              std::cout << "Elem type2: " << cntT2 << "\n";
+              std::cout << "Elem type3: " << cntT3 << "\n";
+              std::cout << "Elem type4: " << cntT4 << "\n";
+              std::cout << "Elem type5: " << cntT5 << "\n";
+              std::cout << "Elem type6: " << cntT6 << "\n";
+              std::cout << "Elem type7: " << cntT7 << "\n";
+            #endif
+            /*rmapX[nr_elem] = msX;
+            rmapY[nr_elem] = msY;
+            if(WindowEvent.key.keysym.sym == SDLK_t)
+            {
+              //+1 pt ca pozitile sunt = 0 (initial)
+              for(int i = 0; i < nr_elem+1; i++)
+              {
+                std::cout << "Test: X= " << rmapX[i] << " | " << "Y= " << rmapY[i] << "\n";
+              }
+            }*/
+            if(WindowEvent.key.keysym.sym == SDLK_RETURN && !finpon && movPos > 0){
               finpon = true;
-              MakeElem(msX, msY);
+              MakeElem(msX[nr_elem], msY[nr_elem]);
               nr_elem++;
             }
             if(finpon) std::cout<<"Is true \n";
               else std::cout<<"Is false \n";
+
+            
         break;
         }
     }
@@ -255,11 +316,11 @@ void circle(int32_t centreX, int32_t centreY,
         }
 
         if (error > 0)
-  {
+        {
           --x;
           tx += 2;
           error += (tx - diameter);
-       }
+        }
     }
        
 }
