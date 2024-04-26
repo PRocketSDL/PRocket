9f58d114f02a4a40377e85204a12401250439fc3
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-04-26 15:16:53 +0300

    Added debug control

--- procket/switch_page.h
+++ procket/switch_page.h
@@ -8,7 +8,7 @@
 #include "textRendering.h"
 
 //All option pages
-#include "map_page.h"
+#include "map_page.hpp"
 #include "settings_page.h"
 
 
@@ -22,7 +22,7 @@ void RenderMainMenu()
     CreateText("Create new map", WINDOW_WIDTH/2, WINDOW_HEIGHT/3+20, OPT_MENU_FONT, 40, create, rcreate);
     CreateText("Settings", WINDOW_WIDTH/2, WINDOW_HEIGHT/3+40, OPT_MENU_FONT, 40, sett, rsett);
     CreateText("Exit", WINDOW_WIDTH/2, WINDOW_HEIGHT/3+60, OPT_MENU_FONT, 40, exitbtn, rexitbtn);
-   
+
     //pentru scrisul rosu
     CreateText("Open existing", WINDOW_WIDTH/3, WINDOW_HEIGHT/2, OPT_MENU_FONT,  40, hopen, ropen, 255, 0, 0, 255);
     CreateText("Create new map", WINDOW_WIDTH/3, WINDOW_HEIGHT/2+20, OPT_MENU_FONT, 40, hcreate, rcreate, 255, 0, 0, 255);
@@ -42,7 +42,9 @@ void ClearMainGraphics()
     SDL_DestroyTexture(hsett);
     SDL_DestroyTexture(exitbtn);
     SDL_DestroyTexture(hexitbtn);
-    std::cout << "[Menu Event] Clear main graphics\n";
+    #ifdef DEBUG_MENU
+        std::cout << "[Menu Event] Clear main graphics\n";
+    #endif
 }
 
 void SwitchPage(int menuState)
@@ -52,13 +54,17 @@ void SwitchPage(int menuState)
     {
         ClearMainGraphics();
         RenderMapPage();
-        std::cout << "[Page Switch Event] Create new map\n";
+        #ifdef DEBUG_MENU
+            std::cout << "[Page Switch Event] Create new map\n";
+        #endif
     }
 
     if(menuState == 3)
     {
        ClearMainGraphics();
        RenderSettingsPage();
-       std::cout << "[Page Switch event] Settings page\n";
+       #ifdef DEBUG_MENU
+           std::cout << "[Page Switch event] Settings page\n";
+       #endif
     }
 }
