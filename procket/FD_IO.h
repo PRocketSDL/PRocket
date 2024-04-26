b3b5bf94084721fce2d37d8dd016f2972cf2de3a
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-04-26 08:33:02 +0300

    Back

--- procket/FD_IO.h
+++ procket/FD_IO.h
@@ -1,7 +1,11 @@
 #pragma once
+
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#if defined _WIN32 || defined _WIN64
+    #include <windows.h>
+#endif
 
 
 
@@ -13,18 +17,19 @@ char rmap_o[StrSize];
 FILE *f;
 
 
+//MARK: Open FD
 void OpenFileDialog()
 {
     //stuff
     #ifdef __linux__
-        f = popen("zenity --file-selection --file-filter='*.rmap' --title='Select rocket map file'", "r");
+        f = popen("zenity --file-selection --file-filter='*.rmap' --filename='.rmap' --title='Select rocket map file'", "r");
         fgets(rmap_i, StrSize, f);
-        #ifdef FD_DEBUG
+        #ifdef DEBUG_FD
             if(strlen(rmap_o) == 0)
-                std::cout << "File selection: none\n";
+                std::cout << "[File Dialog] -> Selection: none\n";
         
             else
-                std::cout << "File selection: " << rmap_i << "\n";
+                std::cout << "[File Dialog] -> Selection: " << rmap_i << "\n";
         #endif
     #endif
 
@@ -33,17 +38,23 @@ void OpenFileDialog()
     #endif
 }
 
+
+//MARK: Save FD
 void SaveFileDialog()
 {
     #ifdef __linux__
-    f = popen("zenity --file-selection --save --file-filter='*.rmap' --title='Save Rocket Map'", "r");
+    f = popen("zenity --file-selection --save --file-filter='*.rmap' --filename='.rmap' --title='Save Rocket Map'", "r");
         fgets(rmap_o, StrSize, f);
-        #ifdef FD_DEBUG
+        #ifdef DEBUG_FD
             if(strlen(rmap_o) == 0)
-                std::cout << "File save: none\n";
-        
+            {
+                std::cout << "[File Dialog] -> Save: none\n";
+            }
             else
-                std::cout << "File save: " << rmap_o << "\n";
+            {
+                //sprintf(rmap_o, "%s", ".rmap");
+                std::cout << "[File Dialog] -> Save: " << rmap_o << "\n";
+            }
         #endif
     #endif
 
