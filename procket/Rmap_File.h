38a41a2fb6805100408ab584723b451d90df04e3
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-05-01 11:39:57 +0300

    Read & Write implemented!

--- procket/Rmap_File.h
+++ procket/Rmap_File.h
@@ -1,15 +1,16 @@
+#pragma once
+
 #include <stdio.h>
 #include <string.h>
 
-//Enabling Lower or upper case chars
-#define MINI_CASE_SENSITIVE
-#include "ext_deps/mINI.hpp"
+
+#include "ext_deps/init/ini.hpp"
 #include "ext_deps/ANSI_Utils.h"
 
 #include "map_page.hpp"
 #include "FD_IO.h"
 
-//Funny Long ;DDD
+
 #define STR_SIZE 55000
 
 
@@ -17,67 +18,73 @@
 //MARK: WriteRMAP
 void WriteRMAP()
 {
-    #ifdef DEBUG_RMAP_FILE
-        std::cout << "[Rmap File Debug] -> Save File Dialog. Waiting For File Selection...\n";
-    #endif
-    SaveFileDialog();
+    init::ini rmapStruct;
 
-    std::string str(rmap_o);
-    mINI::INIFile RmapFile(str);
-    mINI::INIStructure rmapStruct;
-    RmapFile.read(rmapStruct);
-
+    if(strlen(rmap_o) == 0)
+    {
+        #ifdef DEBUG_RMAP_FILE
+            std::cout << "[Rmap File Debug] -> No File Saved. Waiting For File Selection...\n";
+        #endif
+        SaveFileDialog();
+    }
+    
     char ElemCount[STR_SIZE];
-    char RType[STR_SIZE];
-    char RXPos[STR_SIZE];
-    char RYPos[STR_SIZE];
     
     #ifdef DEBUG_RMAP_FILE
         std::cout << "[Rmap File Debug] -> Write\n";
         std::cout << "[Rmap File Debug] -> Element count: " << nr_elem << "\n";
     #endif
     
-
-    //Temp
-    sprintf(ElemCount, "%d", nr_elem);
+    //Write the element count to be parsed latter by ReadRMAP()
+    printf(ElemCount, "%d", nr_elem);
     rmapStruct["Main"]["TotalElemCount"] = ElemCount;
 
     for(int i = 0; i < nr_elem; i++)
     {
         sprintf(ElemCount, "Element_%d", i);
-        sprintf(RType, "%d", E[i].ELtype);
-        sprintf(RXPos, "%d", E[i].x);
-        sprintf(RYPos, "%d", E[i].y);
-
-        rmapStruct[ElemCount]["type"] = RType;
-        rmapStruct[ElemCount]["Xpos"] = RXPos;
-        rmapStruct[ElemCount]["Ypos"] = RYPos;
-
+        rmapStruct[ElemCount]["type"] = E[i].ELtype;
+        rmapStruct[ElemCount]["Xpos"] = E[i].x;
+        rmapStruct[ElemCount]["Ypos"] = E[i].y;
         #ifdef DEBUG_RMAP_FILE
             std::cout << "Type num: " << E[i].ELtype << " | " << "XPos: " << E[i].x << " | " << "YPos: " << E[i].y << "\n";
         #endif
     }
-
-    bool WriteCheck = RmapFile.write(rmapStruct);
-
-    //Chatch rmap file write errors
-    if(!WriteCheck)
-    {
-        XtermFG(FG, 196);
-        std::cout << "Err: ";
-        CresetAll();
-        std::cout << "Failed to write Rocket map file!\n";
-    }
+    init::write_ini(rmapStruct, rmap_o);
     #ifdef DEBUG_RMAP_FILE
-        else
-        {
-            XtermFG(FG, 46);
-            std::cout << "Success: ";
-            CresetAll();
-            std::cout << "Rocket map file written succesfully\n";
-        }
+        std::cout << "[Rmap File Debug] -> File - " << rmap_o << " updated\n";
     #endif
 }
 
 //MARK: ReadRMAP
-//Todo later on...
+void ReadRMAP()
+{
+    #ifdef DEBUG_RMAP_FILE
+        std::cout << "[Rmap File Debug] -> Load rmap file. Waiting for file selection...\n";
+    #endif
+
+    OpenFileDialog();
+
+    const auto rmapStrIn = init::read_ini(rmap_i);
+
+    //The parameters to be stored after parsing the rmap file
+    int InElemCount;
+    int Xin;
+    int Yin;
+    int InType;
+    const auto& MainHeader = rmapStrIn.at("Main");
+    MainHeader.at("TotalElemCount").get_to(InElemCount);
+
+    char ElemSect[STR_SIZE];
+
+    for(int i = 0; i < InElemCount; i++)
+    {
+        sprintf(ElemSect, "Element_%d", i);
+        const auto& ElSects = rmapStrIn.at(ElemSect);
+        ElSects.at("Xpos").get_to(Xin);
+        ElSects.at("Ypos").get_to(Yin);
+        ElSects.at("type").get_to(InType);
+        #ifdef DEBUG_RMAP_FILE
+            std::cout << "[Rmap File Debug] -> Load state" << " Element_" << i << " | Xpos=  " << Xin << " | Ypos= " << Yin << " | type= " << InType << "\n";
+        #endif
+    }
+}
