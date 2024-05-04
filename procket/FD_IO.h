88dd12492ef24f3f38b2cd65f086a1114f29922d
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-05-04 15:05:14 +0300

    Using native file dlg from windows + few bug fixes

--- procket/FD_IO.h
+++ procket/FD_IO.h
@@ -3,8 +3,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+
 #if defined _WIN32 || defined _WIN64
     #include <windows.h>
+    #include <commdlg.h>
+    #include <io.h>
 #endif
 
 
@@ -16,25 +19,60 @@ char rmap_o[StrSize];
 
 FILE *f;
 
+#if defined _WIN32 || defined _WIN64
+    void SymRem(char text[], char symbol, char* clean)
+    {
+        size_t i,j;
+        for(i = 0, j = 0; i < strlen(text); i++)
+        {
+            if(text[i] != symbol)
+            {
+                clean[j++] = text[i];
+            }
+        }
+        clean[j] = '\0';
+    }
+#endif
+
 
 //MARK: Open FD
 void OpenFileDialog()
 {
-    //stuff
     #ifdef __linux__
         f = popen("zenity --file-selection --file-filter='*.rmap' --filename='.rmap' --title='Select rocket map file'", "r");
         fgets(rmap_i, StrSize, f);
         #ifdef DEBUG_FD
             if(strlen(rmap_o) == 0)
                 std::cout << "[File Dialog] -> Selection: none\n";
-        
             else
                 std::cout << "[File Dialog] -> Selection: " << rmap_i << "\n";
         #endif
     #endif
 
     #if defined _WIN32 || defined _WIN64
-        //To do...
+        char FixedPath[260];
+        OPENFILENAME ofn;
+
+        ZeroMemory(&ofn, sizeof(ofn));
+
+        ofn.lStructSize = sizeof(ofn);
+        ofn.lpstrFilter = "Rocket Map Files Files\0*.rmap\0";
+        ofn.lpstrFile = rmap_i;
+        ofn.nMaxFile = sizeof(rmap_i);
+        ofn.lpstrTitle = "Select Rocket Map File";
+        ofn.Flags = OFN_DONTADDTORECENT | OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_EXTENSIONDIFFERENT;
+
+        if(!GetOpenFileName(&ofn))
+        {
+            #ifdef DEBUG_FD
+                std::cout << "[File Dialog] -> Selection: none\n";
+            #endif
+            memset(rmap_i, 0, sizeof(rmap_i));
+        }
+        SymRem(rmap_i, '\"', FixedPath);
+        #ifdef DEBUG_FD
+            std::cout << "[File Dialog] -> Selection: " << rmap_i << "\n";
+        #endif
     #endif
 }
 
@@ -52,13 +90,38 @@ void SaveFileDialog()
             }
             else
             {
-                //sprintf(rmap_o, "%s", ".rmap");
                 std::cout << "[File Dialog] -> Save: " << rmap_o << "\n";
             }
         #endif
     #endif
 
     #if defined _WIN32 || defined _WIN64
-        //To do...
+        char FixedPath[260];
+        OPENFILENAME ofn;
+        //char temPath[StrSize];
+
+        ZeroMemory(&ofn, sizeof(ofn));
+
+        ofn.lStructSize = sizeof(ofn);
+        ofn.lpstrFilter = "Rocket Map Files Files\0*.rmap\0";
+        ofn.lpstrFile = rmap_o;
+        ofn.nMaxFile = sizeof(rmap_o);
+        ofn.lpstrTitle = "Save Rocket Map Files";
+        ofn.Flags = OFN_DONTADDTORECENT | OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+        if(!GetSaveFileName(&ofn))
+        {
+            #ifdef DEBUG_FD
+                std::cout << "[File Dialog] -> Selection: none\n";
+            #endif
+            memset(rmap_o, 0, sizeof(rmap_o));
+        }
+        else
+        {
+            SymRem(rmap_o, '\"', FixedPath);
+            #ifdef DEBUG_FD
+                std::cout << "[File Dialog] -> Selection: " << rmap_o << "\n";
+            #endif
+        }
     #endif
 }
