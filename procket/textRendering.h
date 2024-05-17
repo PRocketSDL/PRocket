746d3b3a85c03670c918cf963d0909e532e85855
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-05-17 14:59:33 +0300

    no more trouble for SDL_ttf

--- procket/textRendering.h
+++ procket/textRendering.h
@@ -7,17 +7,9 @@
 
 
 
-void CreateText(const char* message, int x, int y, const char* font_name,  int font_size, SDL_Texture* &TextTexture, SDL_Rect &TextRect, Uint8 R=255, Uint8 G=255, Uint8 B=255, Uint8 A=255) {
-    TTF_Init();
-    TTF_Font *font = TTF_OpenFont(font_name, font_size);
-    if (!font)
-    {
-        //Some Xterm ansi escape codes to highligh errors easier
-        std::cout << "\x1b[38;5;196m" << font_name << " Not found!" << "\x1b[0m" << std::endl;
-        //If font not found exit instead of remaining stuck (font not found, idk what to do lool)
-        exit(0);
-    }
-    TextSurface = TTF_RenderText_Solid(font, message, {R, G, B, A});
+void CreateText(const char* message, int x, int y, TTF_Font *fnt, int FontSize, SDL_Texture* &TextTexture, SDL_Rect &TextRect, Uint8 R=255, Uint8 G=255, Uint8 B=255, Uint8 A=255) {
+    TTF_SetFontSize(fnt, FontSize);
+    TextSurface = TTF_RenderText_Solid(fnt, message, {R, G, B, A});
     TextTexture = SDL_CreateTextureFromSurface(Renderer, TextSurface);
     TextRect.x = x; // Center horizontaly
     TextRect.y = y; // Center verticaly
@@ -26,8 +18,8 @@ void CreateText(const char* message, int x, int y, const char* font_name,  int f
 
     
     // After you create the texture you can release the surface memory allocation because we actually render the texture not the surface.
-    SDL_FreeSurface(TextSurface);
+    //SDL_FreeSurface(TextSurface);
     
 
-    TTF_Quit();
+    //TTF_Quit();
 }
